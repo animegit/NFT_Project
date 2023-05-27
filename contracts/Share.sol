a7b90424ea49f8d73c1c0296089823e04cf77edb
animegit <ajitanimesh1234@gmail.com>
2023-05-28 03:06:26 +0530

    smart contract for Sharing

--- contracts/Share.sol
+++ contracts/Share.sol
@@ -1,57 +1,109 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.9.0/token/ERC721/utils/ERC721Holder.sol";
-contract Share is ERC20, Ownable, ERC20Permit ,ERC721Holder{
-    IERC721 public collection;
-    uint256 public tokenId;
-    bool public initialized = false;
-    bool public forSale = false;
-    uint256 public salePrice;
-    bool public canRedeem = false;
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+contract Share is ERC721Holder {
+    using SafeMath for uint256;
 
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
-
-    function initialize(address _collection, uint _tokenId,uint256 _amount)external onlyOwner{
-        require(!initialized, "already initialized");
-        require(_amount >0 , "not possible with this much amount");
-        collection = IERC721(_collection);
-        collection.safeTransferFrom(msg.sender, address(this),_tokenId);
-        tokenId = _tokenId;
-        initialized = true;
-        _mint(msg.sender, _amount);
-
+    struct Shareholder {
+        address payable shareholder;
+        uint256 shares;
     }
 
-    function putForSale(uint256 price) external onlyOwner{
-            salePrice = price;
-            forSale = true;
+    struct ShareholderInfo {
+        address shareholder;
+        uint256 shares;
     }
 
-    function purchase() external payable{
-        require(forSale,"not for sale");
-        require(msg.value >= salePrice, "not enough ether sent");
-        collection.transferFrom(address(this),msg.sender, tokenId);
-        forSale = false;
-        canRedeem = true;
+    address public nftAddress;
+    uint256 public totalShares;
+    uint256 public nextShareholderIndex;
+    mapping(uint256 => Shareholder) public shareholders;
+    mapping(address => uint256) public addressToIndex;
+
+    constructor(address _nftAddress) {
+        nftAddress = _nftAddress;
+        totalShares = 0;
+        nextShareholderIndex = 1;
     }
 
-    function redeem(uint256 _amount) external{
-        require(canRedeem,"redemption not available" );
-        uint256 totalEther = address(this).balance;
-        uint256 toRedeem = _amount * totalEther/totalSupply();
+    function addShareholder(address payable _shareholder, uint256 _shares) external {
+        require(_shareholder != address(0), "Invalid shareholder address");
+        require(_shares > 0, "Invalid share value");
 
-        _burn(msg.sender,_amount);
-        payable(msg.sender).transfer(toRedeem);
+        if (addressToIndex[_shareholder] == 0) {
+            addressToIndex[_shareholder] = nextShareholderIndex;
+            shareholders[nextShareholderIndex] = Shareholder(_shareholder, _shares);
+            nextShareholderIndex++;
+        } else {
+            uint256 index = addressToIndex[_shareholder];
+            shareholders[index].shares = shareholders[index].shares.add(_shares);
+        }
+
+        totalShares = totalShares.add(_shares);
     }
-    // function mint(address to, uint256 amount) public onlyOwner {
-    //     _mint(to, amount);
-    // }
 
+    function removeShareholder(address payable _shareholder) external {
+        require(_shareholder != address(0), "Invalid shareholder address");
 
+        uint256 index = addressToIndex[_shareholder];
+        require(index > 0, "Shareholder does not exist");
+
+        uint256 shares = shareholders[index].shares;
+
+        totalShares = totalShares.sub(shares);
+        delete shareholders[index];
+        delete addressToIndex[_shareholder];
+    }
+
+    function shareNFT(uint256 _tokenId) external {
+        IERC721 nft = IERC721(nftAddress);
+        require(nft.ownerOf(_tokenId) == msg.sender, "Caller is not the owner of the NFT");
+
+        uint256 nftShares = shareholders[1].shares;
+        require(nftShares > 0, "No shareholders added");
+
+        nft.safeTransferFrom(msg.sender, address(this), _tokenId);
+
+        uint256 shareValue = nftShares.div(totalShares);
+        for (uint256 i = 1; i < nextShareholderIndex; i++) {
+            address payable shareholder = shareholders[i].shareholder;
+            uint256 shares = shareholders[i].shares;
+            uint256 amount = shareValue.mul(shares);
+            if (amount > 0) {
+                shareholder.transfer(amount);
+            }
+        }
+
+        // Calculate remaining share of the owner
+        uint256 ownerShares = totalShares.sub(nftShares);
+        address payable owner = payable(nft.ownerOf(_tokenId));
+        shareholders[1].shares = ownerShares;
+        shareholders[1].shareholder = owner;
+    }
+
+    function withdrawNFT(uint256 _tokenId) external {
+        IERC721 nft = IERC721(nftAddress);
+        require(nft.ownerOf(_tokenId) == address(this), "Contract does not own the NFT");
+
+        nft.safeTransferFrom(address(this), msg.sender, _tokenId);
+    }
+
+    function getShareholderCount() external view returns (uint256) {
+        return nextShareholderIndex - 1;
+    }
+
+    function getShareholders() external view returns (ShareholderInfo[] memory) {
+        ShareholderInfo[] memory shareholdersInfo = new ShareholderInfo[](nextShareholderIndex - 1);
+
+        for (uint256 i = 1; i < nextShareholderIndex; i++) {
+            Shareholder storage shareholder = shareholders[i];
+            shareholdersInfo[i - 1] = ShareholderInfo(shareholder.shareholder, shareholder.shares);
+        }
+
+        return shareholdersInfo;
+    }
 }
